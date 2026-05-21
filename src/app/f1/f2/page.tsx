272b40584062217b2e6ebcbc21d530eda2cdc818
Dipam Pradhan <meedipam@gmail.com>
2026-05-21 21:57:11 +0545

    intercepting routes

---
+++ src/app/f1/f2/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div>
+      <h1>F2</h1>
+    </div>
+  );
+}
---
+++ src/app/f1/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function () {
+  return (
+    <div>
+      <h1>F1</h1>
+      <Link href="/f1/f2">F2</Link>
+    </div>
+  );
+}
