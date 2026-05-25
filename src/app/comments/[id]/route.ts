6bf928619a6072c3e4f3786056aed0ed65975e82
Dipam Pradhan <meedipam@gmail.com>
2026-05-25 11:43:11 +0545

    Delete Route Handler

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -20,5 +20,26 @@ export async function PATCH(
   if (!comment) {
     return Response.json({ Error: "Comment Not Found" }, { status: 404 });
   }
+  comment.text = text;
   return Response.json(comment);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+  const commentIndex = comments.findIndex(
+    (comment) => comment.id === parseInt(id),
+  );
+  console.log(commentIndex);
+  if (commentIndex === -1) {
+    return Response.json({ Error: "Comment Not Found" }, { status: 404 });
+  }
+  const deletedComment = comments[commentIndex];
+  comments.splice(commentIndex, 1);
+  return Response.json({
+    Note: "Comment Deleted Successfully",
+    deletedComment,
+  });
 }
